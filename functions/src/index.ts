2e45b76b6eea8ccd243bcec3dcb03c24819cd4b0
Mauricio Coelho <mauscoelho@gmail.com>
2018-12-11 00:56:21 +0100

    Add cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,16 @@
 import * as functions from "firebase-functions";
 import getAllViewers from "./getAllViewers";
 
+const cors = require("cors")({ origin: true });
+
 interface Auth {
   username: string;
   password: string;
   index: number;
 }
 
-export const helloWorld = functions.https.onRequest(
-  async (request, response) => {
+export const helloWorld = functions.https.onRequest((request, response) => {
+  return cors(request, response, async () => {
     if (request.method !== "POST") {
       return response.status(401).json({
         message: "Not allowed"
@@ -24,5 +26,5 @@ export const helloWorld = functions.https.onRequest(
     );
 
     return response.send(result);
-  }
-);
+  });
+});
